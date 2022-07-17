7187e0708d2d06326d22e9a3cea9967590c87ee3
Mikolaj Osiak <81968301+ooh-boon-too@users.noreply.github.com>
2022-07-17 17:01:54 +0200

    Update

--- index.js
+++ index.js
@@ -12,173 +12,6 @@ const divScoreEl = document.querySelector('#divScoreEl');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-class Player {
-	constructor(x, y, radius, color) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = color;
-		this.velocity = {
-			x: 0,
-			y: 0
-		};
-	}
-
-	draw() {
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.fillStyle = this.color;
-		c.fill();
-	}
-
-	update() {
-		this.draw();
-
-		const friction = 0.99;
-
-		this.velocity.x *= friction;
-		this.velocity.y *= friction;
-
-		// kolizja dla prawej i lewej
-		if (this.x + this.radius + this.velocity.x <= canvas.width && this.x - this.radius + this.velocity.x >= 0) {
-			this.x += this.velocity.x;
-		} else {
-			this.velocity.x = 0;
-		}
-		// kolizja dla dołu i góry
-		if (this.y + this.radius + this.velocity.y <= canvas.height && this.y - this.radius + this.velocity.y >= 0) {
-			this.y += this.velocity.y;
-		} else {
-			this.velocity.y = 0;
-		}
-	}
-}
-
-class Projectile {
-	constructor(x, y, radius, color, velocity) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = color;
-		this.velocity = velocity;
-	}
-
-	draw() {
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.fillStyle = 'rgba(255,255,255,1)';
-		c.fill();
-	}
-
-	update() {
-		this.draw();
-		this.x += this.velocity.x;
-		this.y += this.velocity.y;
-	}
-}
-
-class Enemy {
-	constructor(x, y, radius, color, velocity) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = color;
-		this.velocity = velocity;
-		this.type = 'Linear';
-		this.radians = 0;
-		this.center = {
-			x,
-			y
-		};
-
-		if (Math.random() < 0.5) {
-			this.type = 'Homing';
-			if (Math.random() < 0.5) {
-				this.type = 'Spinning';
-                if (Math.random() < 0.5) {
-                    this.type = 'Homing Spinning';
-                }
-			}
-		}
-	}
-
-	draw() {
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.fillStyle = this.color;
-		c.fill();
-	}
-
-	update() {
-		this.draw();
-
-		if (this.type === 'Spinning') {
-			this.radians += 0.1;
-
-			this.center.x += this.velocity.x;
-			this.center.y += this.velocity.y;
-
-			this.x = this.center.x + Math.cos(this.radians) * 30;
-			this.y = this.center.y + Math.sin(this.radians) * 30;
-		}
-
-		if (this.type === 'Homing') {
-			const angle = Math.atan2(player.y - this.y, player.x - this.x);
-			this.velocity.x = Math.cos(angle);
-			this.velocity.y = Math.sin(angle);
-
-			this.x = this.x + this.velocity.x;
-			this.y = this.y + this.velocity.y;
-		} else if (this.type === "Homing Spinning") {
-            this.radians += 0.1;
-            const angle = Math.atan2(player.y - this.center.y, player.x - this.center.x);
-
-			this.velocity.x = Math.cos(angle);
-			this.velocity.y = Math.sin(angle);
-
-			this.center.x += this.velocity.x;
-			this.center.y += this.velocity.y;
-
-			this.x = this.center.x + Math.cos(this.radians) * 30;
-			this.y = this.center.y + Math.sin(this.radians) * 30;
-        } else {
-			this.x = this.x + this.velocity.x;
-			this.y = this.y + this.velocity.y;
-		}
-	}
-}
-
-const friction = 0.97;
-class Particle {
-	constructor(x, y, radius, color, velocity) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = color;
-		this.velocity = velocity;
-		this.alpha = 1;
-	}
-
-	draw() {
-		c.save();
-		c.globalAlpha = this.alpha;
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.fillStyle = this.color;
-		c.fill();
-		c.restore();
-	}
-
-	update() {
-		this.draw();
-		this.velocity.x *= friction;
-		this.velocity.y *= friction;
-		this.x = this.x + this.velocity.x;
-		this.y = this.y + this.velocity.y;
-		this.alpha -= 0.01;
-	}
-}
-
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
@@ -190,6 +23,12 @@ let particles = [];
 let animationId;
 let intervalId;
 let score = 0;
+let powerUp = new PowerUp({
+	position: {
+		x: 100,
+		y: 100
+	}
+});
 
 function init() {
 	player = new Player(x, y, 12, 'white');
@@ -226,11 +65,13 @@ function spawnEnemies() {
 }
 
 function animate() {
-	player.update();
 	animationId = requestAnimationFrame(animate);
 	// kolor canvasu
 	c.fillStyle = 'rgba(0, 0, 0, 0.2)';
 	c.fillRect(0, 0, canvas.width, canvas.height);
+
+	player.update();
+	powerUp.update();
 
 	for (let index = particles.length - 1; index >= 0; index--) {
 		const particle = particles[index];
